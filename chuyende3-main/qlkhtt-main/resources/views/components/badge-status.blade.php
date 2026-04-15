50d6cde51c14f7a627b88a899f5ff880015f5ea3
Nguyễn Trà My <20223822@eaut.edu.vn>
2026-04-16 02:01:44 +0700

    Delete chuyende3-main/qlkhtt-main directory

--- chuyende3-main/qlkhtt-main/resources/views/components/badge-status.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['status'])
-
-@if($status == 'published')
-    <span class="badge bg-success">Published</span>
-@else
-    <span class="badge bg-secondary">Draft</span>
-@endif
--- chuyende3-main/qlkhtt-main/resources/views/components/form-file.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="mb-3">
-    <label class="form-label">{{ $label }}</label>
-
-    <input type="file" name="{{ $name }}" class="form-control">
-</div>
